717465c2f8729513ca08807e9e89d80f7ce0e7e1
Jitendra Singh <dba.jitendra@gmail.com>
2020-12-13 08:42:43 +0530

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,9 +2,11 @@
 variable "REGION" {}
 variable "ACCESS_KEY" {}
 variable "SECRET_KEY" {}
+variable "Plan_Name" {}
 
 provider "aws" {
   region     = var.REGION
   access_key = var.ACCESS_KEY
   secret_key = var.SECRET_KEY
+  name = var.Plan_Name
 }
